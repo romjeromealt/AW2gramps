286c2a68f531738c6b22bc088bf76f60caf2c9d6
romjeromealt <romejerome@orange.fr>
2026-08-12 16:47:09 +0200

    Update script_ocr.py
    * Documentation for page segmentation modes
    * use original image for raw tesseract

--- script_ocr.py
+++ script_ocr.py
@@ -702,8 +702,26 @@ def process_image(image_path, config, output_dir):
     print(f"✅ {image_path} → {csv_path} ({len(structured_data)} lignes)")
 
     # Debug
+    #
+    # $ tesseract --help-psm
+    # Page segmentation modes:
+    # 0    Orientation and script detection (OSD) only.
+    # 1    Automatic page segmentation with OSD.
+    # 2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+    # 3    Fully automatic page segmentation, but no OSD. (Default)
+    # 4    Assume a single column of text of variable sizes.
+    # 5    Assume a single uniform block of vertically aligned text.
+    # 6    Assume a single uniform block of text.
+    # 7    Treat the image as a single text line.
+    # 8    Treat the image as a single word.
+    # 9    Treat the image as a single word in a circle.
+    # 10   Treat the image as a single character.
+    # 11   Sparse text. Find as much text as possible in no particular order.
+    # 12   Sparse text with OSD.
+    # 13   Raw line. Treat the image as a single text line bypassing hacks that are Tesseract-specific.
+
     text_pytesseract = pytesseract.image_to_string(processed_col, config=tesseract_config).strip()
-    text_raw = tesseract_ocr_raw(processed_col, lang="eng+fra+osd", psm=6, whitelist=None)
+    text_raw = tesseract_ocr_raw(original_img, lang="eng+fra+osd", psm=11, whitelist=None)
 
     print(f"[DEBUG] pytesseract:\n{text_pytesseract}")
     print(f"[DEBUG] raw tesseract:\n{text_raw}")
